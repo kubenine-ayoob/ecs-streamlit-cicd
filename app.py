8cdb35750e2138bd83954625b8dcf4c901dac09e
kubenine-ayoob <ayoob.h@kubenine.com>
2026-06-15 21:08:00 +0530

    change version in 1.0.1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="ECS Streamlit CI/CD ")
 
 st.title("AWS ECS Streamlit CI/CD ")
 
-st.success("Version 1.0.0")
+st.success("Version 1.0.1")
 
 st.write("Application deployed using ECS Fargate")
 
