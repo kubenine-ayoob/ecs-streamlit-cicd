4b99bdf6518982f4509da43f6616d4e9d72adb7d
kubenine-ayoob <ayoob.h@kubenine.com>
2026-06-15 13:14:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import datetime
+import os
+
+st.set_page_config(page_title="ECS Streamlit CI/CD ")
+
+st.title("AWS ECS Streamlit CI/CD ")
+
+st.success("Version 1.0")
+
+st.write("Application deployed using ECS Fargate")
+
+st.write(f"Current Time: {datetime.datetime.now()}")
+
+st.write(f"Environment: {os.getenv('ENVIRONMENT', 'development')}")
+
+st.info("This application is deployed through GitHub Actions")
